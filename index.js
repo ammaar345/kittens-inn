f047b78c97fc206c4486eaf1589ed73f98efa86e
Ammaar <fakirammaar8@gmail.com>
2020-09-02 14:33:03 +0200

    added filter

--- index.js
+++ index.js
@@ -3,22 +3,24 @@ const express = require("express");
 const exphbs = require("express-handlebars");
 const bodyParser = require("body-parser"); 	// add this line
 const app = express();
+
 const pg = require("pg");
 const Pool = pg.Pool;
-const connectionString = process.env.DATABASE_URL || 'postgresql://codex:pg123@localhost:3009/kitten_inn';
+const connectionString = process.env.DATABASE_URL || 'postgresql://codex:codex123@localhost:5432/kitten_inn';
 
 const pool = new Pool({
-    connectionString
-  });
-  async function queryDB() {
-	
-	const param1 = "value";
+	connectionString
+});
 
-	let results = await pool.query('select field1, field2 from your_table where id = $1', [param1]);
-	
-	return results;
+//   async function queryDB() {
 
-}
+// 	const param1 = "value";
+
+// 	let results = await pool.query('select field1, field2 from your_table where id = $1', [param1]);
+
+// 	return results;
+
+// }
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: false })); // add this line
 app.use(bodyParser.json()); // add  this line
@@ -26,50 +28,59 @@ app.use(bodyParser.json()); // add  this line
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
-const kittens = [
-
-];
 
 // after you added  this  restart the app
-app.get("/", function (req, res) {
-	res.render("index", { kittens });
+app.get("/", async function (req, res) {
+	
+	
+		let kittens = await pool.query('select id ,name,staying_for as days , arriving_on as "arrivingOn" from booking');
+
+		res.render("index", { kittens : kittens.rows  });
+
+
 });
 
 
 
-app.post("/filter", function (req, res) {
+app.post("/filter", async function (req, res) {
 
 	const daysFilter = req.body.daysFilter;
 
-	let filteredData = kittens;
+	let filteredData = [];
 	if (daysFilter === "three") {
-		filteredData = kittens.filter(function(kitten) {
-			return kitten.days <= 3;
-		});
+		const lessThan3='select id ,name ,staying_for as days ,arriving_on as "arrivingOn" from booking where staying_for<=3';
+	const result=await	pool.query(lessThan3)
+	filteredData=result.rows;
+		// filteredData = kittens.filter(function (kitten) {
+		// 	return kitten.days <= 3;
+		// });
 	} else if (daysFilter === "more") {
-		filteredData = kittens.filter(function(kitten) {
-			return kitten.days > 3;
-		});
+		const moreThan3='select id,name,staying_for as days,arriving_on as "arrivingOn" from booking where staying_for>3'
+		// filteredData = kittens.filter(function (kitten) {
+		// 	return kitten.days > 3;
+		// });
+		const result=await pool.query(moreThan3)
+		filteredData=result.rows
+	}
+	else {
+		const all='select id,name ,staying_for as days , arriving_on as "arrivingOn" from booking'
+		result=await pool.query(all);
+		filteredData=result.rows
 	}
 
-	res.render("index", { kittens : filteredData	 });
+	res.render("index", { kittens: filteredData });
 });
 
-app.post("/booking", function (req, res) {
+app.post("/booking", async function (req, res) {
 
 	const days = req.body.days && Number(req.body.days);
 	const name = req.body.name;
 	const arrivingOn = req.body.day;
 	const INSERT_QUERY = "insert into booking (name, staying_for, arriving_on) values ($1, $2, $3)";
 
-pool.query(INSERT_QUERY,[name,days,arrivingOn]);
+	await pool.query(INSERT_QUERY, [name, days, arrivingOn]);
 	if (days && name && arrivingOn) {
-		kittens.push({
-			id : kittens.length+1,
-			days,
-			name,
-			arrivingOn
-		});
+		
 		res.redirect("/");
 
 	} else {
@@ -87,14 +98,14 @@ pool.query(INSERT_QUERY,[name,days,arrivingOn]);
 		});
 
 		const kittenNameInvalid = validate(name, {
-				style: "is-invalid",
-				message: "Enter a valid day"
-			});
+			style: "is-invalid",
+			message: "Enter a valid day"
+		});
 
 		const arrivingOnInvalid = validate(arrivingOn, {
-				style: "is-invalid",
-				message: "Please select a arrival day"
-			});
+			style: "is-invalid",
+			message: "Please select a arrival day"
+		});
 
 
 		res.render("index", {
